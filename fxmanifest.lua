86ae3aa8d59c1e9fcf983610b9b936e0d78a92f8
Polar <84632327+PolarDevv@users.noreply.github.com>
2022-02-11 21:36:51 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+game 'rdr3'
+fx_version 'adamant'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'QB-Menu'
+version '0.0.1'
+
+client_script 'client/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'html/img/*.*',
+}
+
+lua54 'yes'
